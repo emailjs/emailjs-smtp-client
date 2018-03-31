69c6f64b249557b711cea648c843e84baae13602
Felix Hammerl <fhammerl@thoughtworks.com>
2018-03-31 08:37:39 +0200

    Updating dist files

---
+++ dist/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.LOG_LEVEL_ALL = exports.LOG_LEVEL_DEBUG = exports.LOG_LEVEL_INFO = exports.LOG_LEVEL_WARN = exports.LOG_LEVEL_ERROR = exports.LOG_LEVEL_NONE = undefined;
+
+var _common = require('./common');
+
+Object.defineProperty(exports, 'LOG_LEVEL_NONE', {
+  enumerable: true,
+  get: function get() {
+    return _common.LOG_LEVEL_NONE;
+  }
+});
+Object.defineProperty(exports, 'LOG_LEVEL_ERROR', {
+  enumerable: true,
+  get: function get() {
+    return _common.LOG_LEVEL_ERROR;
+  }
+});
+Object.defineProperty(exports, 'LOG_LEVEL_WARN', {
+  enumerable: true,
+  get: function get() {
+    return _common.LOG_LEVEL_WARN;
+  }
+});
+Object.defineProperty(exports, 'LOG_LEVEL_INFO', {
+  enumerable: true,
+  get: function get() {
+    return _common.LOG_LEVEL_INFO;
+  }
+});
+Object.defineProperty(exports, 'LOG_LEVEL_DEBUG', {
+  enumerable: true,
+  get: function get() {
+    return _common.LOG_LEVEL_DEBUG;
+  }
+});
+Object.defineProperty(exports, 'LOG_LEVEL_ALL', {
+  enumerable: true,
+  get: function get() {
+    return _common.LOG_LEVEL_ALL;
+  }
+});
+
+var _client = require('./client');
+
+var _client2 = _interopRequireDefault(_client);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _client2.default;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC5qcyJdLCJuYW1lcyI6WyJMT0dfTEVWRUxfTk9ORSIsIkxPR19MRVZFTF9FUlJPUiIsIkxPR19MRVZFTF9XQVJOIiwiTE9HX0xFVkVMX0lORk8iLCJMT0dfTEVWRUxfREVCVUciLCJMT0dfTEVWRUxfQUxMIl0sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7bUJBR0VBLGM7Ozs7OzttQkFDQUMsZTs7Ozs7O21CQUNBQyxjOzs7Ozs7bUJBQ0FDLGM7Ozs7OzttQkFDQUMsZTs7Ozs7O21CQUNBQyxhOzs7O0FBUkYiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgU210cENsaWVudCBmcm9tICcuL2NsaWVudCdcblxuZXhwb3J0IHtcbiAgTE9HX0xFVkVMX05PTkUsXG4gIExPR19MRVZFTF9FUlJPUixcbiAgTE9HX0xFVkVMX1dBUk4sXG4gIExPR19MRVZFTF9JTkZPLFxuICBMT0dfTEVWRUxfREVCVUcsXG4gIExPR19MRVZFTF9BTExcbn0gZnJvbSAnLi9jb21tb24nXG5cbmV4cG9ydCBkZWZhdWx0IFNtdHBDbGllbnRcbiJdfQ==
